324c56dcb256b10a42529f2feb882d87e2672a95
GabMiglio <69278235+GabMiglio@users.noreply.github.com>
2021-02-23 18:32:16 -0300

    Prototipo de Sistema de Asistencia

---
+++ asistencia.h
@@ -0,0 +1,10 @@
+#ifndef ASISTENCIA_H_INCLUDED
+#define ASISTENCIA_H_INCLUDED
+#include lista.h
+
+void cargarArchivo (tLista &lista);
+void guardarArchivo (tLista & lista);
+
+
+
+#endif // ASISTENCIA_H_INCLUDED
